ed2d92d452eb266d2383cf93bd413875e41c310d
Adam Bourn <abourn@uw.edu>
2017-05-04 15:20:43 -0700

    finish lecture

--- app/src/main/java/edu/uw/sensordemo/MotionActivity.java
+++ app/src/main/java/edu/uw/sensordemo/MotionActivity.java
@@ -1,13 +1,22 @@
 package edu.uw.sensordemo;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MotionActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MotionActivity extends AppCompatActivity implements SensorEventListener {
 
     private static final String TAG = "Motion";
 
@@ -15,7 +24,8 @@ public class MotionActivity extends AppCompatActivity {
 
     private boolean sensorOn;
 
-
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,18 +37,71 @@ public class MotionActivity extends AppCompatActivity {
         txtY = (TextView)findViewById(R.id.txt_y);
         txtZ = (TextView)findViewById(R.id.txt_z);
 
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> sensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (Sensor sensor : sensors) {
+            Log.i(TAG, sensor.toString());
+        }
+
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER); // if running on hardware, change to TYPE_LINEAR_ACCELERATION to account for gravity.
+        // or TYPE_ROTATION_VECTOR to do stuff for like...a ball maze game...
+        if (mSensor == null) {
+            Log.e(TAG, "No accelerometer!");
+            // finish();
+        }
     }
 
+    @Override
+    protected void onPause() {
+        stopSensor(); // every time someone pauses application, we want to stop the sensor to save battery life and what not.
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        startSensor();
+        super.onResume();
+    }
 
     private void startSensor() {
+        // parameters: make this an event listener, register listener on mSensor, rate of when listener should listen
+        mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
         sensorOn = true;
     }
 
     private void stopSensor() {
+        mSensorManager.unregisterListener(this, mSensor); // please stop listening to this sensor
         sensorOn = false;
     }
 
 
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float[] values = event.values; // where you are going to find all the sensings taken by sensor within a sampling
+//        Log.v(TAG, Arrays.toString(values));
+//        txtX.setText(String.format("%.3f", values[0]));
+//        txtY.setText(String.format("%.3f", values[1]));
+//        txtZ.setText(String.format("%.3f", values[2]));
+
+        // for changing the values received from TYPE_ROTATION_VECTOR into roll, pitch, and yaw
+        float[] rotationMatrix = new float[16];
+        SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+
+        float[] orientation = new float[3];
+        SensorManager.getOrientation(rotationMatrix, orientation);
+
+
+        txtX.setText(String.format("%.3f", Math.toDegrees(orientation[1])));
+        txtY.setText(String.format("%.3f", Math.toDegrees(orientation[2])));
+        txtZ.setText(String.format("%.3f", Math.toDegrees(orientation[0])));
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,4 +126,5 @@ public class MotionActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
